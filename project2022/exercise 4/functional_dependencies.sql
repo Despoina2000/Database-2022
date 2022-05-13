43e6a3c0a9c20fe426650349ed1e036fa8f092ce
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2022-05-13 16:04:45 +0300

    Update functional_dependencies.sql

--- project2022/exercise 4/functional_dependencies.sql
+++ project2022/exercise 4/functional_dependencies.sql
@@ -1,10 +1,21 @@
-/*Queries:we are searching the person with more than one gender or names*/
+/*Queries:we are searching the persons with more than one gender or names*/
 SELECT person_id, COUNT (DISTINCT gender), COUNT (DISTINCT name) 
 FROM cast_table
 GROUP BY person_id, gender, name
-HAVING COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1
+HAVING COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1;
 
 SELECT person_id, COUNT (DISTINCT gender), COUNT (DISTINCT name) 
 FROM crew
 GROUP BY person_id, gender, name
-HAVING COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1
+HAVING COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1;
+
+/* DELETE:we are deleting the persons with more than one gender or names*/
+
+DELETE FROM cast_table WHERE COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1;
+
+DELETE FROM crew WHERE COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1;
+
+/*Update*/
+update "Movie_Cast" set character='UNKNOWN' where character IS NULL
+
+/*Alter table: */
