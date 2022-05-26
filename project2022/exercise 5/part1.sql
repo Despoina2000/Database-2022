d1abc8b416c6f38a2332d12e1355e779b0b45d8a
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2022-05-26 21:08:17 +0300

    Update part1.sql

--- project2022/exercise 5/part1.sql
+++ project2022/exercise 5/part1.sql
@@ -1,3 +1,4 @@
+/*creates tables*/
 Create table Person(
     person_id INTEGER  NOT NULL
   ,name      VARCHAR(100) NOT NULL
@@ -17,21 +18,71 @@ Create table CrewMember(
    ,FOREIGN KEY(person_id)  REFERENCES Person(person_id)
 );
 
+/*duplicate*/
+CREATE TABLE Movie_Cast(
+   movie_id  INTEGER  NOT NULL  
+  ,cast_id   INTEGER  NOT NULL
+  ,character VARCHAR(500)
+  ,gender   INTEGER NOT NULL
+  ,person_id INTEGER  NOT NULL
+  ,name      VARCHAR(100) NOT NULL
+  ,FOREIGN key(movie_id) REFERENCES Movie(movie_id)
+);
+
+CREATE TABLE Movie_Crew(
+   movie_id   INTEGER  NOT NULL  
+  ,department VARCHAR(20) NOT NULL
+  ,gender     INTEGER NOT NULL
+  ,person_id  INTEGER  NOT NULL
+  ,job        VARCHAR(100) NOT NULL
+  ,name       VARCHAR(70) NOT NULL
+  ,FOREIGN key(movie_id) REFERENCES Movie(movie_id)
+);
+
+/*drop columns*/
+ALTER TABLE cast_table
+DROP COLUMN name,gender;
+
+ALTER TABLE crew
+DROP COLUMN name,gender;
+
+/*inserts tables*/
 insert into Person
-select gender,person_id,name
-from (SELECT DISTINCT person_id,name,gender
-	  	FROM cast_table 
-		UNION 
-		SELECT DISTINCT person_id,name,gender FROM crew )choice
+select cast_table.person_id, cast_table.name, cast_table.gender
+from cast_table
+union 
+select crew.person_id, crew.name, crew.gender
+from crew
+where not exists (select 1 from cast_table where cast_table.person_id = crew.person_id);
 
 insert into Actor
-select person_id
-from (SELECT DISTINCT person_id
-	  	FROM cast_table 
-		) choice
+select DISTINCT person_id
+from  cast_table; 
 
 insert into Crewmember
-select person_id
-from (SELECT DISTINCT person_id
-	  	FROM crew 
-		) choice
+select distinct person_id
+from crew; 
+
+/*backup*/
+
+insert into Movie_Cast
+select *
+from  cast_table
+
+insert into Movie_Crew
+select *
+from  crew
+
+/*query for 5*/
+    SELECT person_id, COUNT (DISTINCT gender), COUNT (DISTINCT name) 
+FROM person
+GROUP BY person_id
+HAVING COUNT (DISTINCT gender)>1 OR COUNT (DISTINCT name)>1;
+
+/*alter table keys*/
+
+alter table Cast_table
+add FOREIGN KEY (person_id) REFERENCES Person(person_id);
+
+alter table Crew
+add FOREIGN KEY (person_id) REFERENCES Person(person_id);
